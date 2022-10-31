1060b1416de1027782d62b240d45ad8f9168ef67
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-10-30 23:57:49 -0400

    updating js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function btnOver(element) {
-    element.style.backgroundColor = "hsl(232, 10%, 55%)";   
+    // element.style.backgroundColor = "hsl(232, 10%, 55%)"; 
+    element.style.backgroundColor ="hsl(202, 57%, 49%)";
     element.style.color = "white";
+    element.style.borderColor = "hsl(232, 10%, 55%)";   
 }
     
 function btnOut(element) {
     element.style.backgroundColor = "hsl(210, 4%, 67%)";  
     element.style.color = "black";
+    element.style.borderColor = "hsl(210, 4%, 67%)"; 
 }
